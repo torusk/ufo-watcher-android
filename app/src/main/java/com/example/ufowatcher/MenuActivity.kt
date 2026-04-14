942e9e79ead4156b6f1f29071335da49c1d399dc
torusk <shancun.kazuki@gmail.com>
2026-04-14 16:16:46 +0900

    Toast importを復元（openUrl内で使用中）

--- app/src/main/java/com/example/ufowatcher/MenuActivity.kt
+++ app/src/main/java/com/example/ufowatcher/MenuActivity.kt
@@ -5,6 +5,7 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 /**
